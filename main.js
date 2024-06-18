dc49a6aa10707696cdf6fad3c49aef0455fc9d65
AdamField118 <adfield@wpi.edu>
2024-06-18 01:58:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
+  antialiasing: true,
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
